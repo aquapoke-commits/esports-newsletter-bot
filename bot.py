b9d99309374fc69b3f73c106eb68aca89e3a597a
aquapoke-commits <aquapoke@googlemail.com>
2026-02-07 12:37:03 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -53,7 +53,7 @@ SEARCH_KEYWORDS = list(set(PREMIUM_KEYWORDS + NORMAL_KEYWORDS))
 EXCLUDE_LIST = ["theqoo", "더쿠", "instiz", "인스티즈", "fmkorea", "펨코", "dcinside", "디시", "바카라", "토토", "카지노", "슬롯", "MSN", "인벤", "보통주", "패치노트", "사모대출", "investing","vietnam", "ZUM"]
 
 # [설정] ★중복 검사에서만 무시할 단어★ (이 단어들은 겹쳐도 중복으로 안 침)
-IGNORE_DUPLICATE_WORDS = ["Esports", "이스포츠", "e스포츠", "2026", "경기", "리그", "vs", "오늘", "내일", "LCK", "발로란트"]
+IGNORE_DUPLICATE_WORDS = ["Esports", "이스포츠", "e스포츠", "2026", "경기", "리그", "vs", "오늘", "내일", "Insider"]
 
 # [설정] 뉴스 유효 시간
 MAX_HOURS = 24
@@ -409,5 +409,6 @@ async def on_ready():
 
 if __name__ == "__main__":
     bot.run(DISCORD_TOKEN)
+
 
 
