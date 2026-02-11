5695d1476f48abf5771d724ea08b761a994ac6b2
aquapoke-commits <aquapoke@googlemail.com>
2026-02-11 09:35:56 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,11 +38,12 @@ PREMIUM_KEYWORDS = [
 # 🧢 2. 일반 키워드 (2개 이상 있어야 합격)
 NORMAL_KEYWORDS = [
     "Esports", #전문
-    "LoL", "League of Legends", "Valorant", "이터널 리턴", "PUBG", "FC Online", "스타크래프트" #종목명
-    "World Championship", "롤드컵", "MSI", "퍼스트 스탠드", "VCT", "PGS", "PGC", "EWC", "ENC" #국제 대회명
+    "LoL", "League of Legends", "Valorant", "이터널 리턴", "PUBG", "FC Online", "스타크래프트", "나혼렙", "마블 라이벌즈", "리프트바운드", "섀도우버스" #종목명
+    "라이엇", "넷마블", "크래프톤", "블리자드", "넥슨", #종목사
+    "World Championship", "롤드컵", "MSI", "퍼스트 스탠드", "VCT", "PGS", "PGC", "EWC", "ENC", #국제 대회명
     "LCK", "LPL", "LEC", "LCS", "CBLOL", "LCP", #지역 리그명
     "T1", "젠지", "HLE", "한화생명", "DK", "디플러스", "KT", "DRX", "FOX", "NS", "BRO", #이스포츠 팀
-    "학회", "국회", "IESF" #특정 키워드
+    "학회", "국회", "IESF", "이코노미" #특정 키워드
     
 ]
 
@@ -424,6 +425,7 @@ async def on_ready():
 
 if __name__ == "__main__":
     bot.run(DISCORD_TOKEN)
+
 
 
 
